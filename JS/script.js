1e1f380d774528770961d0c8978344060c3fdd60
Ajit Kumar <ajitku425@gmail.com>
2022-06-14 09:47:10 +0530

    Digital Clock

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+function clock()
+{
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let time = "";
+    if(hours < 12)
+    {
+        if(hours == '0')
+        {
+            time = "12" + " : " + minutes + " : " + seconds + " AM";
+        }
+        else
+        {
+            time = hours + " : " + minutes + " : " + seconds + " AM"; 
+        }
+    } 
+    else
+    {
+        if(hours == '12')
+        {
+            time = "12" + " : " + minutes + " : " + seconds + " PM";
+        }
+        else
+        {
+            hours = hours - 12;
+            time = hours + " : " + minutes + " : " + seconds + " AM"; 
+        }
+    }
+    document.getElementById('time').innerHTML = time;
+}
+setInterval(clock, 1000);
